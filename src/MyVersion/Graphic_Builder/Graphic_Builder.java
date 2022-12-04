701ea09ba87501f0e09ba5955bedec202f21dee4
Tito226 <104071611+Tito226@users.noreply.github.com>
2022-12-04 21:30:59 +0200

    Graphic builder start

--- src/MyVersion/Graphic_Builder/Graphic_Builder.java
+++ src/MyVersion/Graphic_Builder/Graphic_Builder.java
@@ -5,19 +5,35 @@ import java.awt.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Graphic_Builder {
+    static int heigh=1400;
+    static int weight=500;
     public static void main(String[] args) {
+
+        int weight=500;
         JFrame frame=new JFrame("Frame");
-        frame.setSize(300, 200);
+        frame.setSize(weight, heigh);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
         Graphics g= frame.getGraphics();
         while(true){
-
+        paint(0.1f,g);
         }
 
     }
 
-   float testFunc(float x){
+    static float testFunc(float x){
         return x*x;
    }
+
+   static void paint(float step, Graphics g){
+        float i=0f;
+        int y1=heigh/2;
+        int x1=0;
+       while(x1<weight && testFunc(i)<heigh){
+           g.drawLine(x1,y1,x1+3,(int)testFunc(i));
+           x1+=3;
+           i+=step;
+       }
+   }
+
 }
