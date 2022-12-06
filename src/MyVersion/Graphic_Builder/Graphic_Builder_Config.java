441a794be791f15c6c343a11d37dd741690050ea
Timurs <chashka1come1home@gmail.com>
2022-12-06 22:38:42 +0200

    gr builder 2

---
+++ src/MyVersion/Graphic_Builder/Graphic_Builder_Config.java
@@ -0,0 +1,7 @@
+package MyVersion.Graphic_Builder;
+
+public class Graphic_Builder_Config {
+    public static final int INDENT_X =50;//отступ
+    public static final int INDENT_Y =200;//move graphic down on this value
+    public static final int INDENT_BETWEEN_GRAPHIC_COORDS=1;//DONT USE IT
+}
